245adbb8d0619d0e0474d90da16779604219b6ca
cloud <62519659+lem6ns@users.noreply.github.com>
2023-01-15 19:50:01 -0700

    feat(api): add /maps/txt route

--- src/routes/api/txt/txt.ts
+++ src/routes/api/txt/txt.ts
@@ -1,4 +1,6 @@
 import { FastifyPluginAsync, FastifyRequest } from "fastify";
+import { maps } from "../../../util/getAllMaps";
+import { getTxt } from "../../../util/parser";
 import * as config from "../../../../config.json";
 
 const txt: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
@@ -7,7 +9,16 @@ const txt: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
 	}>;
 
 	fastify.get("/:id", async function (request: Request, reply) {
-		return request.params.id;
+		const map = maps[request.params.id];
+		if (!map) {
+			reply.status(404);
+			return {
+				error: "022-310",
+				info: "Map with requested ID does not exist",
+			};
+		}
+
+		return getTxt(map);
 	});
 };
 
